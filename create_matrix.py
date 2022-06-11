69b8af47989d93b49119ddd6b40a14e19db38748
Anita Liberatore <anitaliberatore@hotmail.com>
2022-06-11 16:55:59 +0200

    add file create matrix read from file

---
+++ create_matrix.py
@@ -0,0 +1,21 @@
+import random
+import numpy as np
+# This function create a matrix with random value beetwen 0 a 1 
+# Function with params: row and column
+
+def create_matrix(my_row, my_column):
+    matrix = [[random.randint(0, 1) for _ in range(my_row)] for _ in range(my_column)]
+
+    for row in matrix:
+        for item in row:
+            print(item, end=" ")
+        print()
+    print("")
+
+def create_matrix_from_file():
+    f = open ('input.txt' , 'r')
+    matrix = []
+    matrix = [line.split() for line in f]
+    matrix=np.array(matrix)
+    matrix=matrix.astype(np.int)
+    return matrix
