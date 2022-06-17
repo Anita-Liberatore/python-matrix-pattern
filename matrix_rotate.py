067175413ed71e2857fec940cbf2204c72a7b324
Anita Liberatore <anitaliberatore@hotmail.com>
2022-06-17 19:18:01 +0200

    add 270 degree rotate

--- matrix_rotate.py
+++ matrix_rotate.py
@@ -8,10 +8,35 @@ def rotate90Clockwise(A):
             A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i]
             A[j][N - 1 - i] = temp
     return A
- 
+
+def rotate180Matrix(mat):     
+    N = len(mat[0])
+    # rotate the matrix by 180 degrees
+    for i in range(N // 2):
+        for j in range(N):
+            temp = mat[i][j]
+            mat[i][j] = mat[N - i - 1][N - j - 1]
+            mat[N - i - 1][N - j - 1] = temp
+
+    return mat
+
+def rotare270Matrix(mat):
+    new_matrix = []
+    for i in range(len(mat[0]), 0, -1):
+        new_matrix.append(list(map(lambda x: x[i-1], mat)))
+
+    return new_matrix
 
  
 def operation(pattern,degree):
     if degree == '90':
         pattern_90 = rotate90Clockwise(pattern)
-    return pattern_90
+        return pattern_90
+    elif degree == '180':
+        pattern_180 = rotate180Matrix(pattern)
+        return pattern_180
+    elif degree == '270':
+        pattern_270 = rotare270Matrix(pattern)
+        return pattern_270
+    else:
+        return "No case"
